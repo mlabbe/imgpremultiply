582fd46edce59d131aeff79bd189da698d3d1b67
Michael Labbe <mike@frogtoss.com>
2015-06-17 20:37:30 -0700

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# Premultiply Images #
+
+Simply premultiply the alpha of images.  It's written in Python but runs quickly because it uses Numpy.
+
+Tested on Windows with Python 3.
+
+Windows users can download precompiled dependencies.
+
+- [http://www.numpy.org/](Numpy)
+- [https://python-pillow.github.io/](Pillow)  The friendly PIL fork for Python 3.
+
+Usage:
+
+    python3 imgpremultiply.py *.png
+
---
+++ imgpremultiply.py
@@ -0,0 +1,39 @@
+# premultiply the alpha of images
+# does not check if the images are already premultiplied (there is no good way to do this)
+# works in place, so have a backup
+#
+# python 3-friendly
+# tested with Pillow for PIL, precompiled Numpy lib
+
+from PIL import Image
+import sys
+import glob
+import numpy
+
+def premultiply(file_list):
+    for path in file_list:
+        im = Image.open(path).convert('RGBA')
+        a = numpy.fromstring(im.tostring(), dtype=numpy.uint8)
+        alphaLayer = a[3::4] / 255.0
+        a[::4]  *= alphaLayer
+        a[1::4] *= alphaLayer
+        a[2::4] *= alphaLayer
+
+        im = Image.fromstring("RGBA", im.size, a.tostring())
+        im.save(path)
+        print(path)
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print("usage: %s <list of files to process>" % sys.argv[0])
+        sys.exit(1)
+
+    # check for un-expanded wildcard (windows does this)
+    if sys.argv[1].find('*') != -1:
+        file_list = glob.glob(sys.argv[1])
+    else:
+        file_list = sys.argv[1:]
+
+    premultiply(file_list)
+    sys.exit(0)
